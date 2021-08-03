155b37dc72a4dd26fd3b2d605ddaa6027cb684dd
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-08-03 19:59:43 +0300

    added controllers result

---
+++ controllers/questions/getTestResult.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line object-curly-spacing
+const {tests: service} = require('../../services')
+
+const getTestResult = async (req, res, next) => {
+  //   const answers = req.body //будет приходить массив объектов(id вопроса, ответ на вопрос)
+  const answers = [
+    {
+      _id: '6107d49073048a242080648e',
+      userAnswer:
+        'Answers the question whether the product is being created correctly in terms of customer expectations',
+    },
+    {
+      _id: '6107d49073048a242080648f',
+      userAnswer: 'All requirements must be known at the beginning of the project life cycle',
+    },
+  ]
+  try {
+    const questions = await service.getAll()
+
+    const answerId = answers.map(answer => answer._id)
+    const answerUser = answers.map(answer => answer.userAnswer)
+
+    const rightAnswers = questions
+      .filter(question => answerId.includes(question.id))
+      .filter(question => answerUser.includes(question.rightAnswer))
+
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        rightAnswers: rightAnswers,
+      },
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = getTestResult
--- routes/api/tests.js
+++ routes/api/tests.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { questions: ctrl } = require('../../controllers')
+const {questions: ctrl} = require('../../controllers')
 
-router.get('/:questionType', ctrl.getTests)
+router.get('/all/:questionType', ctrl.getTests)
 
 router.post('/', express.json(), ctrl.addOne)
 
 router.post('/many', express.json(), ctrl.addMany)
+
+router.get('/result', ctrl.getTestResult)
 
 module.exports = router
